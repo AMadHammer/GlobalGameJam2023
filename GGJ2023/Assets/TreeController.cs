f357f66b91550c215c32d5e0bf80108f6c68668a
Ammar Ahmed <ammarcola@gmail.com>
2023-02-05 15:34:09 -0500

    tree cont4roller

---
+++ GGJ2023/Assets/TreeController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeController : MonoBehaviour
+{
+
+    public bool BucketsCollected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (BucketsCollected)
+        {
+            if (transform.localScale.y < 1)
+            {
+                var temp = transform.localScale;
+                temp.y += 0.001f;
+                transform.localScale = temp;
+            }
+        }    
+    }
+
+
+    
+}
